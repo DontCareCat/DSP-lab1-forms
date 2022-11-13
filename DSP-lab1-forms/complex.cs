cda2f6abfd4408d204eeb64d0320e0cfdc64d22d
DontCareCat <zhenya21012001@gmail.com>
2022-11-13 17:46:59 +0200

    fft implemented

---
+++ DSP-lab1-forms/complex.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSP_lab1_forms
+{
+    class complex
+    {
+        public double real;
+        public double imag;
+        public complex()
+        {
+            real = 0;
+            imag = 0;
+        }
+        public complex(double real, double imag)
+        {
+            this.real = real;
+            this.imag = imag;
+        }
+        public override string ToString()
+        {
+            return $"{real} {imag}i";
+        }
+        public static complex fromPolar(double r, double rad)
+        {
+            return new complex(r * Math.Cos(rad), r * Math.Sin(rad));
+        }
+        public static complex operator +(complex a, complex b)
+        {
+            return new complex(a.real + b.real, a.imag + b.imag);
+        }
+        public static complex operator -(complex a,complex b)
+        {
+            return new complex(a.real - b.real, a.imag - b.imag);
+        }
+        public static complex operator *(complex a, complex b)
+        {
+            return new complex(a.real*b.real - a.imag*b.imag, a.real*b.imag + a.imag*b.real);
+        }
+        public double magnit
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow(real,2)+Math.Pow(imag,2));
+            }
+        }
+        public double phase
+        {
+            get
+            {
+                return Math.Atan(imag / real);
+            }
+        }
+    }
+}
---
+++ DSP-lab1-forms/fourier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSP_lab1_forms
+{
+    static class fourier
+    {
+        public static complex[] FFT(complex[] x)
+        {
+            int N = x.Length;
+            complex[] X = new complex[N];
+            complex[] d, D, e, E;
+            if(N==1)
+            {
+                X[0] = x[0];
+                return X;
+            }
+            int k;
+            e = new complex[N / 2];
+            d = new complex[N / 2];
+
+            for(k=0;k<N/2;k++)
+            {
+                e[k] = x[2 * k];
+                d[k] = x[2 * k + 1];
+            }
+            D = FFT(d);
+            E = FFT(e);
+            for(k=0;k<N/2;k++)
+            {
+                X[k] = E[k] + D[k];
+                X[k + N / 2] = E[k] - D[k];
+            }
+            return X;
+        }
+    }
+}
